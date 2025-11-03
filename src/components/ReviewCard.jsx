5f55dd0597b875fc0305c7d9d08a5058f5dca950
RiccardoOttavi <ottavi.ps@libero.it>
2025-11-03 15:12:43 +0100

    inserisci la pagina delle review

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,16 @@
+// creazione componente Header
+const ReviewCard = () => {
+    return (
+        <div>
+            <div>
+                <p>
+                    testo della recensione
+                </p>
+                <strong>Vote: 5</strong>
+                <address><i>By nome utente</i></address>
+            </div>
+        </div>
+    )
+}
+
+export default ReviewCard
--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,5 +1,26 @@
+import ReviewCard from "../components/ReviewCard"
+import { Link } from "react-router"
+
 export default function DetailsPage(){
     return (
-        <h2>Ciao</h2>
+         <>
+            <header>
+                <h1>Titolo Film</h1>
+                <h3><i>By Nome regista</i></h3>
+                <p>lorem ipsm dolor sit amet</p>
+            </header>
+            <section>
+                <header>
+                    <h4>Our community reviews</h4>
+                </header>
+                <ReviewCard />
+                <ReviewCard />
+                <ReviewCard />
+            </section>
+            <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
+                <Link className="btn btn-secondary" to="/">Back to home</Link>
+            </footer>
+
+        </>
     )
 }
