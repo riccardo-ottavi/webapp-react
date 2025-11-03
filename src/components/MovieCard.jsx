0d3a675d80bd4b83348be3788df5cbd10b50c1b2
RiccardoOttavi <ottavi.ps@libero.it>
2025-11-03 15:04:12 +0100

    crea la card dei film

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,23 @@
+// import di router-dom per link
+import { Link } from "react-router-dom"
+
+// creazione componente Header
+const MovieCard = () => {
+    return (
+        <div>
+            <img src="https://tse2.mm.bing.net/th/id/OIP.vnJImFIy1GEoBBAjyZ-tfQHaK-?rs=1&pid=ImgDetMain&o=7&rm=3" alt="titolo film" />
+            <div>
+                <h5 className="card-title">Titolo Film</h5>
+                <address><i>Regista</i></address>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quam! Quaerat molestias autem hic neque quia quod asperiores, vel veniam mollitia repellat illo repudiandae praesentium qui deserunt velit adipisci explicabo!
+                </p>
+                <Link to="movies/1">
+                    See more
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
+import MovieCard from "../components/MovieCard"
+
 export default function HomePage() {
     return(
         <>
             <h1>Bool Movies</h1>
             <h2>The nerdest movie community</h2>
+            <div>
+                <MovieCard />
+            </div>
         </>
     )
 }
