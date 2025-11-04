80a30c8ac1fb607d82a127b827fd4470806dfb18
RiccardoOttavi <ottavi.ps@libero.it>
2025-11-04 15:15:53 +0100

    inserisci le props

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,28 +5,35 @@ import  { useState, useEffect } from "react"
 
 export default function HomePage() {
 
-    const [movies, setMovies] = useState();
+    const [movies, setMovies] = useState([]);
 
     // prepariamo funzione per la chiamata axios
-    const fecthMovies = () => {
+    const fetchMovies = () => {
         axios.get('http://localhost:3000/movies')
             .then(response => { setMovies(response.data) })
             .catch(error => { console.log(error) })
     }
 
-    // faccio partire la chiamata a primo montaggio comp
-    useEffect(fecthMovies, []);
+    // funzione di generazione istanze libri
+    const renderMovies = () => {
+        return movies.map(movie => {
+            return (
+                <div className="col" key={movie.id}>
+                    <MovieCard movieProp={movie} />
+                </div>
+            )
+        })
+    }
 
-    return (
+    // faccio partire la chiamata a primo montaggio comp
+    useEffect(fetchMovies, []);
+
+     return (
         <>
             <h1>Bool Movies</h1>
-            <h2><i>The nerdest book community</i></h2>
-            <div className="row row-cols-3 mt-4">
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+            <h2><i>The nerdest movie community</i></h2>
+            <div>
+                {renderMovies()}
             </div>
         </>
 
